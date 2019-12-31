9549bc78feeb57a4145203894a5d9012c1f70a72
Stephen Charbonneau <schar138@uottawa.ca>
2019-12-30 21:34:41 -0500

    modified decimalTobb26

--- src/decimalTobb26.ts
+++ src/decimalTobb26.ts
@@ -6,7 +6,11 @@
  * @example decimalTobb26(497) //SC
  */
 export default function decimalTobb26(input: number): string {
-    if (/^(0|([1-9]\d*))$/.test(input.toString()) && typeof input === "number")
+    if (
+        !/^(0|([1-9]\d*))$/.test(input.toString()) ||
+        typeof input !== "number" ||
+        input === 0
+    )
         throw new TypeError(
             "Input number may only contain natural numbers values"
         );
@@ -14,7 +18,7 @@ export default function decimalTobb26(input: number): string {
 }
 
 function rdecimalTobb26(input: number, sum: string = ""): string {
-    return input % 26 !== 0
+    return input > 1
         ? rdecimalTobb26(
               input / 26,
               String.fromCharCode(64 + (input % 26)) + sum
