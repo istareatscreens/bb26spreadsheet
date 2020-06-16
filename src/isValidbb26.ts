d5fc3f87a50c2030fee24c826c844c8abbac0b69
istareatscreens <schar138@uottawa.ca>
2020-06-16 01:02:48 -0400

    Fixed spelling mistake in method

--- src/isValidbb26.ts
+++ src/isValidbb26.ts
@@ -2,7 +2,7 @@
  * Checks for valid bb26 format, capital letters with no spaces. Throws
  * TypeError on invalid input
  * @param {input} input string containing only uppercase letters
- * @param {boolean} surpressError controls if an error is thrown if not set
+ * @param {boolean} suppress controls if an error is thrown if not set
  *     throws error, if set to true throws no error and returns false
  * @return {boolean} returns true if valid or if surpresserror not set to true
  *     then throws error
@@ -13,10 +13,12 @@
  * @Example isValidbb26("A1") //Throws TypeError
  */
 export function checkIfValidbb26(
-    input: string, surpressError: boolean = false): boolean {
+  input: string,
+  suppress: boolean = false
+): boolean {
   if (!/^[A-Z]+$/.test(input)) {
-    if (surpressError) return false;
-    throw new TypeError('Input string may only contain upper case letters');
+    if (suppress) return false;
+    throw new TypeError("Input string may only contain upper case letters");
   }
   return true;
 }
