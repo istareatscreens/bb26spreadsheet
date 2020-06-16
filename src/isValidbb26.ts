bc48962f2f2a0da439ecc5114d825b8713d82679
istareatscreens <schar138@uottawa.ca>
2020-06-16 01:04:07 -0400

    Fixed commenting on method

--- src/isValidbb26.ts
+++ src/isValidbb26.ts
@@ -4,7 +4,7 @@
  * @param {input} input string containing only uppercase letters
  * @param {boolean} suppress controls if an error is thrown if not set
  *     throws error, if set to true throws no error and returns false
- * @return {boolean} returns true if valid or if surpresserror not set to true
+ * @return {boolean} returns true if valid or if suppressError not set to true
  *     then throws error
  * @Example isValidbb26("AA") //Execution continues returns true
  * @Example isValidbb26("Aa", true) //returns false
